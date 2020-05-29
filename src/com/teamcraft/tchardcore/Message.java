6ec4226b82f4bc4ea2f19eecf96abd50b98d950d
Jeremy Noesen <jeremynoesen@gmail.com>
2020-05-29 18:09:18 -0500

    Messages class fixed up

--- src/com/teamcraft/tchardcore/Message.java
+++ src/com/teamcraft/tchardcore/Message.java
@@ -1,7 +1,8 @@
 package com.teamcraft.tchardcore;
 
-import jndev.volleyball.config.ConfigType;
-import jndev.volleyball.config.Configs;
+import com.teamcraft.tchardcore.config.Config;
+import com.teamcraft.tchardcore.config.ConfigType;
+import com.teamcraft.tchardcore.config.Configs;
 import org.bukkit.ChatColor;
 
 /**
@@ -9,65 +10,24 @@ import org.bukkit.ChatColor;
  */
 public class Message {
     
-    private static jndev.volleyball.config.Config messageConfig = Configs.getConfig(ConfigType.MESSAGE);
+    private static final Config messageConfig = Configs.getConfig(ConfigType.MESSAGE);
     
     public static String PREFIX;
-    public static String ERROR_BALL_OUT;
-    public static String ERROR_NOT_ON_COURT;
-    public static String ERROR_NULL_BOUNDS;
-    public static String ERROR_UNKNOWN_COURT;
-    public static String SUCCESS_COURT_REMOVED;
-    public static String SUCCESS_SET_COURT_BOUNDS;
-    public static String SUCCESS_COURT_CREATED;
-    public static String SUCCESS_SET_COURT_ENABLED;
-    public static String ERROR_CANT_ENABLE;
-    public static String SUCCESS_SET_COURT_SPEED;
-    public static String SUCCESS_SET_COURT_TEXTURE;
-    public static String SUCCESS_COURT_SELECTED;
-    public static String SUCCESS_SET_NET_BOUNDS;
-    public static String SUCCESS_SET_COURT_ANIMATIONS;
-    public static String ERROR_NO_PERMS;
-    public static String ERROR_UNKNOWN_ARGS;
-    public static String ERROR_DEFAULT;
-    public static String SUCCESS_RELOADED;
-    public static String COURT_INFO;
-    public static String COURT_LIST;
+    public static String PVP_ENABLED;
+    public static String PVP_DISABLED;
+    public static String CANT_HURT;
+    public static String CHECK_TIME;
     
+    /**
+     * loads/reloads messages from file
+     */
     public static void reloadMessages() {
         PREFIX = format(messageConfig.getConfig().getString("PREFIX"));
-        ERROR_BALL_OUT = PREFIX + format(messageConfig.getConfig().getString("ERROR_BALL_OUT"));
-        ERROR_NOT_ON_COURT = PREFIX + format(messageConfig.getConfig().getString("ERROR_NOT_ON_COURT"));
-        ERROR_NULL_BOUNDS = PREFIX + format(messageConfig.getConfig().getString("ERROR_NULL_BOUNDS"));
-        ERROR_UNKNOWN_COURT = PREFIX + format(messageConfig.getConfig().getString("ERROR_UNKNOWN_COURT"));
-        SUCCESS_COURT_REMOVED = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_COURT_REMOVED"));
-        SUCCESS_SET_COURT_BOUNDS = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_SET_COURT_BOUNDS"));
-        SUCCESS_COURT_CREATED = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_COURT_CREATED"));
-        SUCCESS_SET_COURT_ENABLED = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_SET_COURT_ENABLED"));
-        ERROR_CANT_ENABLE = PREFIX + format(messageConfig.getConfig().getString("ERROR_CANT_ENABLE"));
-        SUCCESS_SET_COURT_SPEED = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_SET_COURT_SPEED"));
-        SUCCESS_SET_COURT_TEXTURE = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_SET_COURT_TEXTURE"));
-        SUCCESS_COURT_SELECTED = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_COURT_SELECTED"));
-        SUCCESS_SET_NET_BOUNDS = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_SET_NET_BOUNDS"));
-        SUCCESS_SET_COURT_ANIMATIONS = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_SET_COURT_ANIMATIONS"));
-        ERROR_NO_PERMS = PREFIX + format(messageConfig.getConfig().getString("ERROR_NO_PERMS"));
-        ERROR_UNKNOWN_ARGS = PREFIX + format(messageConfig.getConfig().getString("ERROR_UNKNOWN_ARGS"));
-        ERROR_DEFAULT = PREFIX + format(messageConfig.getConfig().getString("ERROR_DEFAULT"));
-        SUCCESS_RELOADED = PREFIX + format(messageConfig.getConfig().getString("SUCCESS_RELOADED"));
-        COURT_INFO = PREFIX + format(messageConfig.getConfig().getString("COURT_INFO"));
-        COURT_LIST = PREFIX + format(messageConfig.getConfig().getString("COURT_LIST"));
+        PVP_ENABLED = PREFIX + format(messageConfig.getConfig().getString("PVP_ENABLED"));
+        PVP_DISABLED = PREFIX + format(messageConfig.getConfig().getString("PVP_DISABLED"));
+        CANT_HURT = PREFIX + format(messageConfig.getConfig().getString("CANT_HURT"));
+        CHECK_TIME = PREFIX + format(messageConfig.getConfig().getString("CHECK_TIME"));
     }
-    
-    public static String STARTUP = "\n\n" +
-            ChatColor.DARK_GRAY + "███╗" + ChatColor.YELLOW + "██╗   ██╗" + ChatColor.GOLD + "██████╗ " + ChatColor.DARK_GRAY + "███╗" + ChatColor.WHITE + "  Volleyball version " + VolleyBall.getInstance().getDescription().getVersion() + " " + "has " + "been enabled!\n" +
-            ChatColor.DARK_GRAY + "██╔╝" + ChatColor.YELLOW + "██║   ██║" + ChatColor.GOLD + "██╔══██╗" + ChatColor.DARK_GRAY + "╚██║" + ChatColor.WHITE + "  Volleyball is written by Jeremaster101 and\n" +
-            ChatColor.DARK_GRAY + "██║ " + ChatColor.YELLOW + "██║   ██║" + ChatColor.GOLD + "██████╔╝" + ChatColor.DARK_GRAY + " ██║" + ChatColor.WHITE + "  may not be modified or redistributed without\n" +
-            ChatColor.DARK_GRAY + "██║ " + ChatColor.YELLOW + "╚██╗ ██╔╝" + ChatColor.GOLD + "██╔══██╗" + ChatColor.DARK_GRAY + " ██║" + ChatColor.WHITE + "  his consent. For help and support, join the\n" +
-            ChatColor.DARK_GRAY + "███╗" + ChatColor.YELLOW + " ╚████╔╝ " + ChatColor.GOLD + "██████╔╝" + ChatColor.DARK_GRAY + "███║" + ChatColor.WHITE + "  support discord group: https://discord.gg/WhmQYR\n" +
-            ChatColor.DARK_GRAY + "╚══╝" + ChatColor.YELLOW + "  ╚═══╝  " + ChatColor.GOLD + "╚═════╝ " + ChatColor.DARK_GRAY + "╚══╝" + ChatColor.WHITE + "  Thank you for choosing Volleyball!\n";
-    public static String CLEANING = PREFIX + ChatColor.GRAY + "Deleting leftover volleyball entities...";
-    public static String DONE_CLEANING =
-            PREFIX + ChatColor.GRAY + "Successfully deleted " + ChatColor.WHITE + "$COUNT$" + ChatColor.GRAY +
-                    " volleyball entities!";
     
     /**
      * Apply color codes and line breaks to a message
@@ -79,39 +39,30 @@ public class Message {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
     
-    public static String[] HELP_MAIN = new String[]{
+    /**
+     * help message
+     */
+    public static String[] HELP = new String[]{
             "",
-            ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "-------------[" + ChatColor.YELLOW +
-                    "" + ChatColor.BOLD + "Volley" + ChatColor.GOLD +
-                    "" + ChatColor.BOLD + "Ball " + ChatColor.GRAY +
-                    "" + ChatColor.BOLD + "Help"
+            ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "-------------[" + PREFIX +
+                    ChatColor.GRAY + "" + ChatColor.BOLD + "Help"
                     + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "]-------------",
-            ChatColor.GRAY + "/volleyball court help" + ChatColor.WHITE + ": View court commands",
-            ChatColor.GRAY + "/volleyball reload" + ChatColor.WHITE + ": Reload plugin and configs",
+            ChatColor.GRAY + "/pvptime" + ChatColor.WHITE + ": View time remaining on your pvp timer",
             ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "---------------------------------------",
             ""
     };
     
-    public static String[] HELP_COURT = new String[]{
+    /**
+     * help message for admins
+     */
+    public static String[] ADMIN_HELP = new String[]{
             "",
-            ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "----------[" + ChatColor.YELLOW +
-                    "" + ChatColor.BOLD + "Volley" + ChatColor.GOLD +
-                    "" + ChatColor.BOLD + "Ball " + ChatColor.GRAY +
-                    "" + ChatColor.BOLD + "Court Help"
-                    + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "]----------",
-            ChatColor.GRAY + "/volleyball court list" + ChatColor.WHITE + ": List all courts",
-            ChatColor.GRAY + "/volleyball court <courtname> create" + ChatColor.WHITE + ": Create a new court",
-            ChatColor.GRAY + "/volleyball court <courtname> remove" + ChatColor.WHITE + ": Remove a court",
-            ChatColor.GRAY + "/volleyball court <courtname> info" + ChatColor.WHITE + ": Get court info",
-            ChatColor.GRAY + "/volleyball court <courtname> set net" + ChatColor.WHITE + ": Set the net region",
-            ChatColor.GRAY + "/volleyball court <courtname> set bounds" + ChatColor.WHITE + ": Change the court bounds",
-            ChatColor.GRAY + "/volleyball court <courtname> set animations <true/false>" + ChatColor.WHITE + ": Enable ball animations",
-            ChatColor.GRAY + "/volleyball court <courtname> set speed <number>" + ChatColor.WHITE + ": Set ball speed",
-            ChatColor.GRAY + "/volleyball court <courtname> set texture <url>" + ChatColor.WHITE + ": Set ball texture",
-            ChatColor.GRAY + "/volleyball court <courtname> set enabled <true/false>" + ChatColor.WHITE + ": Enable a court",
-            
+            ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "-------------[" + PREFIX +
+                    ChatColor.GRAY + "" + ChatColor.BOLD + "Help"
+                    + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "]-------------",
+            ChatColor.GRAY + "/pvptime" + ChatColor.WHITE + ": View time remaining on your pvp timer",
+            ChatColor.GRAY + "/tchcreload" + ChatColor.WHITE + ": Reload all files for plugin",
             ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "---------------------------------------",
             ""
     };
-    
 }
